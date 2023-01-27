f72fadaa2c15fea4c8c75122cccfccc0726251e6
Satish <satish.p@ensologic.com>
2023-01-27 11:45:51 +0530

    googlefontissue

--- criativcity/pages/_document.js
+++ criativcity/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+   return (
+      <Html lang="en">
+         <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
+         </Head>
+         <body>
+            <Main />
+            <NextScript />
+         </body>
+      </Html>
+   );
 }
