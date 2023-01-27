63ba4e331899c63db446f7a2362432a1311afff8
Satish <satish.p@ensologic.com>
2023-01-27 11:43:06 +0530

    nextcrossorfix

--- criativcity/pages/index.js
+++ criativcity/pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
             <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
          </Head>
          <div className=" bg-black-gradient w-full overflow-hidden">
