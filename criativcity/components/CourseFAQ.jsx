7df5e2c2c7da35e5ad01df6720384944b197bbdd
Satish <satish.p@ensologic.com>
2023-01-29 03:05:12 +0530

    toggleadded

--- criativcity/components/CourseFAQ.jsx
+++ criativcity/components/CourseFAQ.jsx
@@ -5,6 +5,15 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import { slideIn, staggerContainer, textVariant, textVariant2 } from "@/utils/motion";
 const CourseFAQ = () => {
+   const[show,setShow] =useState({})
+
+   function handleShow(id) {
+      setShow((prev) => {
+          return {...prev, [id]: !prev[id]};
+      });
+    }
+    
+  
    return (
       <section className="flex items-center  flex-col justify-center pt-[3rem] lg:pt-[8rem] pb-4">
          <div className="w-[60%] flex text-center items-center justify-center">
@@ -15,18 +24,30 @@ const CourseFAQ = () => {
               With our course, you'll learn how to
             </h2>
          </div>
-         <div className="faqqna lg:space-y-5 space-y-2">
-            <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3 ">
-               <h3 className="text-white text-[0.7rem] lg:text-[1.8rem] font-Lato">Master industry-standard software, "Adobe Premiere Pro"</h3>
+         <div className="faqqna lg:space-y-5 space-y-2 mt-3 ">
+            <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3 " onClick={()=>{handleShow(1)}}>
+               <h3 className="text-white text-[1.1rem] lg:text-[1.8rem] font-Lato">Master industry-standard software, "Adobe Premiere Pro"</h3>
+               {show[1] &&(<div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+               </div>)}
+            </div>
+            <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3" onClick={()=>{handleShow(2)}}>
+               <h3 className="text-white text-[1.2rem] lg:text-[1.8rem] font-Lato">Understand the Psychology behind Editing</h3>
+               {show[2] &&(<div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+                  <h6>- Edit and enhance video footage </h6>
+               </div>)}
             </div>
             <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3">
-               <h3 className="text-white text-[0.7rem] lg:text-[1.8rem] font-Lato">Understand the Psychology behind Editing</h3>
+               <h3 className="text-white text-[1.2rem] lg:text-[1.8rem] font-Lato">Decoding Youtubers and industries</h3>
             </div>
             <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3">
-               <h3 className="text-white text-[0.7rem] lg:text-[1.8rem] font-Lato">Decoding Youtubers and industries</h3>
-            </div>
-            <div className="border-2 rounded-lg border-fuchsia-50 px-4 py-3">
-               <h3 className="text-white text-[0.7rem] lg:text-[1.8rem] font-Lato">Businesses With Editing</h3>
+               <h3 className="text-white text-[1.2rem] lg:text-[1.8rem] font-Lato">Businesses With Editing</h3>
             </div>
          </div>
       </section>
