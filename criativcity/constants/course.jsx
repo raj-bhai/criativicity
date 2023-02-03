35e1a7db9909ce55bfa1126e53fca7148b964baf
Raj <raj.kiran@ensologic.com>
2023-02-03 12:41:11 +0530

    course-data

---
+++ criativcity/constants/course.jsx
@@ -0,0 +1,266 @@
+export const course = [
+    {
+        courseName: "Adobe Premiere Pro CC Masterclass: Full Crash Course.",
+        details: [
+            "24.5 hours on-demand video",
+            "10 hours 33 Minutes Left",
+            "Creator  : Yasir Quyoom",
+            "Last updated 08/2022"
+        ],
+        videos: [
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            },
+            {
+                title: "Adobe Premiere Pro Essentials Course",
+                duration: "10 minutes",
+                thumbnail: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fiverr-training-youtube-thumbnail-template-design-51c6e3b67f402eb6ba444cbd1ec82185_screen.jpg?ts=1641552258",
+                url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                completed: "true",
+                like: 25,
+                description: "In this video, We are explaining about Introduction to Video Editing. Please do watch the complete video for in-depth information.",
+                comments: [
+                    {
+                        user: "Yasir Quyoom",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    },
+                    {
+                        user: "Rajkiran Kalwar",
+                        data: "I have one Question How to animate AI Vectors in Premiere pro",
+                        like: 29,
+                        reply: []
+                    }
+                ]
+            }
+        ]
+    }
+];
