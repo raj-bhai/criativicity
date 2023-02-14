ca1383d4447b44dde6182a15d98c8196461f585b
Manish <manish.kumar@ensologic.com>
2023-02-14 10:23:33 +0530

    finishing

--- criativcity/pages/login/index.jsx
+++ criativcity/pages/login/index.jsx
@@ -20,16 +20,16 @@ const LoginScreen = () => {
                         <form action='' className='flex flex-col gap-4 text-white '>
                             <div className='relative'>
                                 <span>EMAIL</span>
-                                <input className='p-2 w-full' type="text" name="email" placeholder='name@email.com' autoComplete='off' />
+                                <input className='p-2 w-full text-gray-500' type="text" name="email" placeholder='name@email.com' autoComplete='off' />
                             </div>
                             <div className='relative'>
                                 <span>PASSWORD</span>
-                                <input className='p-2 w-full' type="password" name="password" placeholder='Enter Your Password' autoComplete='off'
+                                <input className='p-2 w-full text-gray-500' type="password" name="password" placeholder='Enter Your Password' autoComplete='off'
                                 />
                                 <BsEye className='absolute top-1/2 right-3 text-black' width="16" height="16" />
                             </div>
                             <span className='text-dimPink'>Forget Password?</span>
-                            <button className='bg-gradient-to-r from-dimPink to-dimPurple text-white py-2 px-3'>Login</button>
+                            <button className='logibtn text-white py-2 px-3'>Login</button>
                         </form>
                         <div className='mt-5 grid grid-cols-3 items-center text-dimPink'>
                             <hr className='border-gray-400' />
@@ -37,15 +37,15 @@ const LoginScreen = () => {
                             <hr className='border-gray-400' />
                         </div>
                         <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm text-white'><FcGoogle width="25px" height="25px" className='mr-8 text-white' />Continue with Google</button>
-                        <button className='border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm text-white'><BsFacebook width="25px" height="25px" className='mr-8 text-white' />Continue with Facebook</button>
-                        <button className='border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm text-white'><BsApple width="25px" height="25px" className='mr-8' />Continue with Apple</button>
+                        {/* <button className='border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm text-white'><BsFacebook width="25px" height="25px" className='mr-8 text-white' />Continue with Facebook</button>
+                        <button className='border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm text-white'><BsApple width="25px" height="25px" className='mr-8' />Continue with Apple</button> */}
                         <div className='mt-3 text-xs flex justify-center items-center text-white'>
                             <p>New to Coursera?</p>
                             <button className='py-2 hover:scale-110 duration-300 ml-2'><a href='signup'>Sign up</a></button>
                         </div>
                         <hr className='border-white' />
                         <div className='justify-center items-center text-sm text-white'>
-                            <p className='text-dimPink'>Log in with your organization</p>
+                            {/* <p className='text-dimPink'>Log in with your organization</p> */}
                             <br></br>
                             <p className='text-white'>Having trouble logging in?<u>Learner Help center</u></p>
                             <br />
--- criativcity/pages/signup/index.jsx
+++ criativcity/pages/signup/index.jsx
@@ -17,20 +17,20 @@ const SignupScreen = () => {
                         <form action='' className='flex flex-col gap-4'>
                             <div className='relative'>
                                 <span>FULL NAME</span>
-                                <input className='p-2 w-full' type="text" name="name" placeholder='Enter Your FullName' autoComplete='off' />
+                                <input className='p-2 w-full text-gray-500' type="text" name="name" placeholder='Enter Your FullName' autoComplete='off' />
                             </div>
                             <div className='relative'>
                                 <span>EMAIL</span>
-                                <input className='p-2 w-full' type="text" name="email" placeholder='name@email.com' autoComplete='off' />
+                                <input className='p-2 w-full text-gray-500' type="text" name="email" placeholder='name@email.com' autoComplete='off' />
                             </div>
                             <div className='relative'>
                                 <span>PASSWORD</span>
-                                <input className='p-2 w-full' type="text" name="password" placeholder='Enter Your Password' autoComplete='off'
+                                <input className='p-2 w-full text-gray-500' type="password" name="password" placeholder='Enter Your Password' autoComplete='off'
                                 />
                                 <BsEye className='absolute top-1/2 right-3 -translate-y-1/2' width="16" height="16" />
                             </div>
                             <span className='text-dimPink'>Forget Password?</span>
-                            <button className='bg-gradient-to-r from-dimPink to-dimPurple text-white py-2 px-3'>Login</button>
+                            <button className='logibtn text-white py-2 px-3'>Sign Up</button>
                         </form>
                         <div className='mt-5 grid grid-cols-3 items-center text-dimPink'>
                             <hr className='border-gray-400' />
@@ -38,15 +38,15 @@ const SignupScreen = () => {
                             <hr className='border-gray-400' />
                         </div>
                         <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm'><FcGoogle width="25px" height="25px" className='mr-8' />Continue with Google</button>
-                        <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm'><BsFacebook width="25px" height="25px" className='mr-8' />Continue with Facebook</button>
-                        <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm'><BsApple width="25px" height="25px" className='mr-8' />Continue with Apple</button>
+                        {/* <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm'><BsFacebook width="25px" height="25px" className='mr-8' />Continue with Facebook</button>
+                        <button className=' border-dimPink border-2 py-2 px-3 items-center w-full mt-4 flex justify-center text-sm'><BsApple width="25px" height="25px" className='mr-8' />Continue with Apple</button> */}
                         <div className='mt-3 text-xs flex justify-center items-center'>
                             <p>New to Coursera?</p>
                             <button className='py-2 hover:scale-110 duration-300 ml-2'><a href='signup'>Sign up</a></button>
                         </div>
                         <hr className='border-white' />
                         <div className='justify-center items-center text-sm'>
-                            <p className='text-dimPink'>Log in with your organization</p>
+                            {/* <p className='text-dimPink'>Log in with your organization</p> */}
                             <br></br>
                             <p className='text-white'>Having trouble logging in?<u>Learner Help center</u></p>
                             <br />
