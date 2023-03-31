78cad415708603d42e90c35a01c8756a80e62cfe
Raj <raj.kiran@ensologic.com>
2023-03-31 11:45:15 +0530

    shimmer in course

--- criativcity/components/CourseComp.jsx
+++ criativcity/components/CourseComp.jsx
@@ -10,12 +10,18 @@ import * as CourseAction from "../redux/action/course";
 import { useRouter } from "next/router";
 import axios from "axios";
 import { Apiurl } from "@/constants/url";
+import Shimmer from "./courseShimmer";
 
 const CourseComp = () => {
    const dispatch = useDispatch();
    const router = useRouter();
    const Courses = useSelector((state) => state.course.CourseList);
-   const userDetail = useSelector((state) => state.user.UserDetail)
+   const userDetail = useSelector((state) => state.user.UserDetail);
+   const [domLoaded, setDomLoaded] = useState(false);
+
+   useEffect(() => {
+      setDomLoaded(true)
+   }, [])
 
    useEffect(() => {
       if (!Courses?.length) {
@@ -92,65 +98,6 @@ const CourseComp = () => {
       });
    }
 
-   // const handlePayment = async () => {
-   //    try {
-   //       const response = await fetch("../api/payment", {
-   //          method: "POST",
-   //          headers: {
-   //             "Content-Type": "application/json",
-   //          },
-   //          body: JSON.stringify({
-   //             "amount": 100,
-   //             "currency": "INR",
-   //             "receipt": "receipt_1",
-   //             "Fpayment_capture": 1,
-   //          }),
-   //       });
-   //       const data = await response.json();
-   //       if (response.ok) {
-   //          console.log("ok")
-   //       } else {
-   //          console.log("not ok")
-   //       }
-   //    } catch (err) {
-   //       console.log("error :", err)
-   //    }
-   // };
-
-
-
-   // const Payment = () => {
-   //    console.log("payment initiated")
-
-   //    const options = {
-   //       key: "YOUR_KEY_ID",
-   //       amount: 10000, // Amount in paise
-   //       currency: "INR",
-   //       name: "My Company",
-   //       description: "Test Transaction",
-   //       image: "https://via.placeholder.com/150",
-   //       order_id: "order_EdZe6xyiuVOlIq",
-   //       handler: function (response) {
-   //          console.log("Payment response :", response)
-   //          // setPaymentStatus("success");
-   //       },
-   //       prefill: {
-   //          name: "John Doe",
-   //          email: "johndoe@example.com",
-   //          contact: "9999999999",
-   //       },
-   //       notes: {
-   //          address: "Razorpay Corporate Office",
-   //       },
-   //       theme: {
-   //          color: "#3399cc",
-   //       },
-   //    };
-
-   //    const rzp = new Razorpay(options);
-   //    rzp.open();
-   // };
-
    const onClickCourse = async () => {
       const token = await localStorage.getItem('token');
       if (!token) {
@@ -164,23 +111,25 @@ const CourseComp = () => {
 
    return (
       <>
-         <SkeletonTheme baseColor="#ebebeb" highlightColor="#898989">
-            {Courses?.length ? (
-               <section className=" text-white navbarbg">
-                  <div className="text-center lg:text-left lg:ml-[10rem] mt-[4rem] py-12">
-                     <h1
-                        className={`font-pressfont text-[1.5rem] lg:text-[3.1rem]  
+         {/* <SkeletonTheme baseColor="#ebebeb" highlightColor="#898989"> */}
+         {
+            domLoaded &&
+            <section className=" text-white navbarbg">
+               <div className="text-center lg:text-left lg:ml-[10rem] mt-[4rem] py-12">
+                  <h1
+                     className={`font-pressfont text-[1.5rem] lg:text-[3.1rem]  
                         pressFont text-transparent bg-clip-text bg-gradient-to-r from-dimPink to-dimPurple`}
-                     >
-                        Courses
-                     </h1>
-                  </div>
+                  >
+                     Courses
+                  </h1>
+               </div>
+               {Courses?.length ? (
                   <div className="flex items-center navbarbg justify-center">
                      {/* <Skeleton height={460}></Skeleton> */}
                      {Courses.map((item, index) => (
                         <section
                            key={index}
-                           className="mt flex justify-center cursor-pointer hover:bg-[#17202A] border-2 border-fuchsia-50 rounded-lg lg:w-[78%] px-2 mx-2 py-1"
+                           className="mt flex justify-center cursor-pointer hover:bg-[#17202A] border-2 border-fuchsia-500 rounded-lg lg:w-[78%] px-2 mx-2 py-1"
                            onClick={() => {
                               onClickCourse()
                            }}
@@ -232,119 +181,125 @@ const CourseComp = () => {
                         </section>
                      ))}
                   </div>
+               ) :
+                  <div className="flex items-center navbarbg justify-center">
+                     <Shimmer />
+                  </div>
+               }
 
-                  <div className="more-course navbarbg flex flex-col items-center justify-center mt-24">
-                     <div className="text">
-                        <h2
-                           className={`font-pressfont text-[1.5rem] lg:text-[3.1rem]  
+               <div className="more-course navbarbg flex flex-col items-center justify-center mt-24">
+                  <div className="text">
+                     <h2
+                        className={`font-pressfont text-[1.5rem] lg:text-[3.1rem]  
                pressFont text-transparent bg-clip-text bg-gradient-to-r from-dimPink to-dimPurple`}
-                        >
-                           More Courses Coming Soon
-                        </h2>
-                     </div>
-                     <div className="border-2 border-fuchsia-50 rounded-lg">
-                        <input className="outline-none px-2 py-3 bg-transparent" placeholder="Enter Your Email" type="text" />
-                        <button className="px-5 border-2 mx-1 rounded-lg py-2 text-fuchsia-400 ">Submit</button>
-                     </div>
-                     <div className="faqqna navbarbg lg:space-y-5 space-y-2 mt-3 pt-12 ">
-                        <div
-                           className=" rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3 "
-                           onClick={() => {
-                              handleShow(1);
-                           }}
-                        >
-                           <h3 className="text-white relative flex items-center justify-between text-[0.9rem] lg:text-[1.8rem]  font-Lato">
-                              <h2 className="w-[90%] lg:w-[95%] mr-1"> Master industry-standard software, &quot;Adobe Premiere Pro&quot;</h2>
-                              {show[1] ? (
-                                 <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
-                              ) : (
-                                 <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
-                              )}
-                           </h3>
+                     >
+                        More Courses Coming Soon
+                     </h2>
+                  </div>
+                  <div className="border-2 border-fuchsia-50 rounded-lg">
+                     <input className="outline-none px-2 py-3 bg-transparent" placeholder="Enter Your Email" type="text" />
+                     <button className="px-5 border-2 mx-1 rounded-lg py-2 text-fuchsia-400 ">Submit</button>
+                  </div>
+                  <div className="faqqna navbarbg lg:space-y-5 space-y-2 mt-3 pt-12 ">
+                     <div
+                        className=" rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3 "
+                        onClick={() => {
+                           handleShow(1);
+                        }}
+                     >
+                        <h3 className="text-white relative flex items-center justify-between text-[0.9rem] lg:text-[1.8rem]  font-Lato">
+                           <h2 className="w-[90%] lg:w-[95%] mr-1"> Master industry-standard software, &quot;Adobe Premiere Pro&quot;</h2>
+                           {show[1] ? (
+                              <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
+                           ) : (
+                              <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
+                           )}
+                        </h3>
 
-                           {show[1] && (
-                              <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                              </div>
+                        {show[1] && (
+                           <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                           </div>
+                        )}
+                     </div>
+                     <div
+                        className="rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
+                        onClick={() => {
+                           handleShow(2);
+                        }}
+                     >
+                        <h3 className="text-white text-[0.9rem] flex items-center justify-between lg:text-[1.8rem] font-Lato">
+                           Understand the Psychology behind Editing
+                           {show[2] ? (
+                              <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
+                           ) : (
+                              <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
                            )}
-                        </div>
-                        <div
-                           className="rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
-                           onClick={() => {
-                              handleShow(2);
-                           }}
-                        >
-                           <h3 className="text-white text-[0.9rem] flex items-center justify-between lg:text-[1.8rem] font-Lato">
-                              Understand the Psychology behind Editing
-                              {show[2] ? (
-                                 <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
-                              ) : (
-                                 <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
-                              )}
-                           </h3>
-                           {show[2] && (
-                              <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                              </div>
+                        </h3>
+                        {show[2] && (
+                           <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                           </div>
+                        )}
+                     </div>
+                     <div
+                        className="rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
+                        onClick={() => {
+                           handleShow(3);
+                        }}
+                     >
+                        <h3 className="text-white text-[0.9rem] items-center flex justify-between lg:text-[1.8rem] font-Lato">
+                           Decoding Youtubers and industries
+                           {show[3] ? (
+                              <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
+                           ) : (
+                              <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
                            )}
-                        </div>
-                        <div
-                           className="rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
-                           onClick={() => {
-                              handleShow(3);
-                           }}
-                        >
-                           <h3 className="text-white text-[0.9rem] items-center flex justify-between lg:text-[1.8rem] font-Lato">
-                              Decoding Youtubers and industries
-                              {show[3] ? (
-                                 <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
-                              ) : (
-                                 <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
-                              )}
-                           </h3>
-                           {show[3] && (
-                              <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                              </div>
+                        </h3>
+                        {show[3] && (
+                           <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                           </div>
+                        )}
+                     </div>
+                     <div
+                        className=" rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
+                        onClick={() => {
+                           handleShow(4);
+                        }}
+                     >
+                        <h3 className="text-white text-[0.9rem] items-center flex justify-between lg:text-[1.8rem] font-Lato">
+                           Businesses With Editing
+                           {show[4] ? (
+                              <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
+                           ) : (
+                              <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
                            )}
-                        </div>
-                        <div
-                           className=" rounded-lg border-[0.04rem] border-fuchsia-400 px-4 py-3"
-                           onClick={() => {
-                              handleShow(4);
-                           }}
-                        >
-                           <h3 className="text-white text-[0.9rem] items-center flex justify-between lg:text-[1.8rem] font-Lato">
-                              Businesses With Editing
-                              {show[4] ? (
-                                 <IoIosArrowUp size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowUp>
-                              ) : (
-                                 <IoIosArrowDown size={45} className="border-[0.04rem] rounded-md  border-fuchsia-400 px-2 py-2"></IoIosArrowDown>
-                              )}
-                           </h3>
-                           {show[4] && (
-                              <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                                 <h6>- Edit and enhance video footage </h6>
-                              </div>
-                           )}
-                        </div>
+                        </h3>
+                        {show[4] && (
+                           <div className="text-left text-white font-Lato lg:text-[1.2rem] text-[0.9rem]">
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                              <h6>- Edit and enhance video footage </h6>
+                           </div>
+                        )}
                      </div>
                   </div>
-               </section>
-            ) : null}
-         </SkeletonTheme>
+               </div>
+            </section>
+
+         }
+         {/* </SkeletonTheme> */}
       </>
    );
 };
---
+++ criativcity/components/courseShimmer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './Shimmer.css';
+
+const Shimmer = () => {
+    return (
+        <div className="shimmer-wrapper border-2 border-fuchsia-500 rounded-lg lg:w-[78%] px-2 mx-2 py-1 h-[500px] ">
+            <div className="shimmer"></div>
+        </div>
+    );
+};
+
+export default Shimmer;
