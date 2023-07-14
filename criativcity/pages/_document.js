39ec715550c878828fe7993e76daec0e386ad65c
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-14 23:21:50 +0530

    sdk

--- criativcity/pages/_document.js
+++ criativcity/pages/_document.js
@@ -1,26 +1,35 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-   return (
+class MyDocument extends Document {
+  render() {
+    return (
       <Html lang="en">
-         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
-         </Head>
-         <body>
-            <Main />
-            <NextScript />
-            <script
-               type="module"
-               src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"
-               async
-            ></script>
-            <script
-               nomodule
-               src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.js"
-               defer
-            ></script>
-         </body>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
+          <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            type="module"
+            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"
+            async
+          ></script>
+          <script
+            nomodule
+            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.js"
+            defer
+          ></script>
+          <script
+            type="text/javascript"
+            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js"
+          ></script>
+        </body>
       </Html>
-   );
+    );
+  }
 }
+
+export default MyDocument;
