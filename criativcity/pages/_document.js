77dc45b26bd92090823abc65047372928b5544b8
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-14 23:32:59 +0530

    sdk-fix

--- criativcity/pages/_document.js
+++ criativcity/pages/_document.js
@@ -4,29 +4,17 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
-          <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-          <script
-            type="module"
-            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"
-            async
-          ></script>
-          <script
-            nomodule
-            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.js"
-            defer
-          ></script>
-          <script
-            type="text/javascript"
-            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js"
-          ></script>
-        </body>
+         <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
+            <script type="module" src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"></script>
+            <script nomodule src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js"></script>
+            <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
+         </Head>
+         <body>
+            <Main />
+            <NextScript />
+         </body>
       </Html>
     );
   }
--- criativcity/pages/payment/index.js
+++ criativcity/pages/payment/index.js
@@ -5,36 +5,33 @@ const CheckoutPage = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const loadBillDeskSDK = () => {
-      const script = document.createElement('script');
-      script.src = 'https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js';
-      script.async = true;
-      script.onload = launchBillDeskSDK;
-
-      document.body.appendChild(script);
-    };
-
     const launchBillDeskSDK = () => {
+      // Retrieve the query parameter value
       const dataParam = router.query.data;
 
       try {
+        // Attempt to parse the value as JSON
         const data = JSON.parse(dataParam);
 
         const logo = 'https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png';
         const flowType = 'payments';
         const childWindow = true;
-        const retryCount = 3;
-        const returnUrl = 'https://criativcity.com/';
-        const prefs = ['category1', 'category2', 'category3'];
-        const bdOrderId = data.orderid;
-        const merchantId = data.mercid;
-        const authToken = data.links[1].headers.authorization;
+        const retryCount = 3; // Number of retry attempts
+        const returnUrl = 'https://criativcity.com/'; // Merchant return URL
+        const prefs = ['category1', 'category2', 'category3']; // Payment categories in preferred order
+        const bdOrderId = data.orderid; // Order ID generated using Create Order API
+        const merchantId = data.mercid; // Merchant ID received from BillDesk
+        const authToken = data.links[1].headers.authorization; // Authorization token generated using Create Order API
 
         const responseHandler = (transactionMetadata) => {
+          // Callback function to receive transaction metadata after completion of the transaction journey
           console.log('Transaction Metadata:', transactionMetadata);
+
           // Process the transaction metadata and handle the payment completion as needed
+          // Redirect the user to a success page or trigger any necessary actions
         };
 
+        // Call the BillDesk SDK function to launch the checkout experience
         if (typeof window !== 'undefined' && window.BillDeskSDK && typeof window.BillDeskSDK.invokePaymentSDK === 'function') {
           window.BillDeskSDK.invokePaymentSDK(
             logo,
@@ -51,16 +48,22 @@ const CheckoutPage = () => {
         } else {
           console.error('BillDeskSDK.invokePaymentSDK is not available');
         }
+
       } catch (error) {
         console.error('Error parsing JSON:', error);
       }
     };
 
-    if (typeof window !== 'undefined' && !window.BillDeskSDK) {
-      loadBillDeskSDK();
-    } else {
-      launchBillDeskSDK();
-    }
+    const loadBillDeskSDK = () => {
+      const script = document.createElement('script');
+      script.src = 'https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js';
+      script.async = true;
+      script.onload = launchBillDeskSDK;
+
+      document.body.appendChild(script);
+    };
+
+    loadBillDeskSDK();
   }, [router.query.data]);
 
   return (
