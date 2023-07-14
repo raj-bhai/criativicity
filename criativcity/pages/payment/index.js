11f8141849262c87248b5ccb600b3e91ff7804e5
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-14 22:42:21 +0530

    sdk

--- criativcity/pages/payment/index.js
+++ criativcity/pages/payment/index.js
@@ -5,36 +5,39 @@ const CheckoutPage = () => {
   const router = useRouter();
 
   useEffect(() => {
+    const loadBillDeskSDK = () => {
+      const script = document.createElement('script');
+      script.src = 'https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js';
+      script.async = true;
+      script.onload = launchBillDeskSDK;
+
+      document.body.appendChild(script);
+    };
+
     const launchBillDeskSDK = () => {
-      // Retrieve the query parameter value
       const dataParam = router.query.data;
 
       try {
-        // Attempt to parse the value as JSON
         const data = JSON.parse(dataParam);
 
         const logo = 'https://www.logodesign.net/logo/line-art-house-roof-and-buildings-4485ld.png';
         const flowType = 'payments';
         const childWindow = true;
-        const retryCount = 3; // Number of retry attempts
-        const returnUrl = 'https://criativcity.com/'; // Merchant return URL
-        const prefs = ['category1', 'category2', 'category3']; // Payment categories in preferred order
-        const bdOrderId = data.orderid        ; // Order ID generated using Create Order API
-        const merchantId = data.mercid; // Merchant ID received from BillDesk
-        const authToken = data.links[1].headers.authorization; // Authorization token generated using Create Order API
+        const retryCount = 3;
+        const returnUrl = 'https://criativcity.com/';
+        const prefs = ['category1', 'category2', 'category3'];
+        const bdOrderId = data.orderid;
+        const merchantId = data.mercid;
+        const authToken = data.links[1].headers.authorization;
 
         const responseHandler = (transactionMetadata) => {
-          // Callback function to receive transaction metadata after completion of the transaction journey
           console.log('Transaction Metadata:', transactionMetadata);
-
           // Process the transaction metadata and handle the payment completion as needed
-          // Redirect the user to a success page or trigger any necessary actions
         };
 
-        // Call the BillDesk SDK function to launch the checkout experience
-        if (window.BillDeskSDK && typeof window.BillDeskSDK.invokePaymentSDK === 'function') {
+        if (typeof window !== 'undefined' && window.BillDeskSDK && typeof window.BillDeskSDK.invokePaymentSDK === 'function') {
           window.BillDeskSDK.invokePaymentSDK(
-            logoUrl,
+            logo,
             flowType,
             childWindow,
             retryCount,
@@ -48,25 +51,17 @@ const CheckoutPage = () => {
         } else {
           console.error('BillDeskSDK.invokePaymentSDK is not available');
         }
-
       } catch (error) {
         console.error('Error parsing JSON:', error);
       }
     };
 
-    // Load the BillDesk SDK script dynamically
-    const loadBillDeskSDK = () => {
-      const script = document.createElement('script');
-      script.src = 'https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js';
-      script.async = true;
-      script.onload = launchBillDeskSDK;
-
-      document.body.appendChild(script);
-    };
-
-    // Call the function to load the BillDesk SDK when the component mounts
-    loadBillDeskSDK();
-  }, []);
+    if (typeof window !== 'undefined' && !window.BillDeskSDK) {
+      loadBillDeskSDK();
+    } else {
+      launchBillDeskSDK();
+    }
+  }, [router.query.data]);
 
   return (
     <div>
