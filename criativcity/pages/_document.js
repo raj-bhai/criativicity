8f1a7989f05ac69a0cafd6c3b4672d469f3484ef
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-15 00:00:28 +0530

    build

--- criativcity/pages/_document.js
+++ criativcity/pages/_document.js
@@ -1,23 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-         <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
-            <script type="module" src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.esm.js"></script>
-            <script nomodule src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js"></script>
-            <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
-         </Head>
-         <body>
-            <Main />
-            <NextScript />
-         </body>
+      <Html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
+          <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
+          <script
+            async
+            type="module"
+            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"
+          ></script>
+          <script
+            async
+            nomodule
+            src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.js"
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
 }
-
-export default MyDocument;
