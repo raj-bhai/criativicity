54dd36812b69b4e41d9a174399ccb29c9be8850d
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-15 13:08:48 +0530

    jjj

--- criativcity/pages/checkout/index.jsx
+++ criativcity/pages/checkout/index.jsx
@@ -3,10 +3,10 @@ import CheckoutComp from '@/components/checkout';
 import Navbar from "@/components/Navbar"
 import Layout from './layout';
 import Footer from "@/components/Footer"
-import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js';
+// import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe('pk_live_51MvZaGSB8NyItH1JpBDZEV9GCj1Ait4hUzXZLT0ZFOG3hbCr9EWEXMb6urkhAIEJ8W0e7FfLLTbqvZsQ8v4LUV8W00hOPtM781');
+// const stripePromise = loadStripe('pk_live_51MvZaGSB8NyItH1JpBDZEV9GCj1Ait4hUzXZLT0ZFOG3hbCr9EWEXMb6urkhAIEJ8W0e7FfLLTbqvZsQ8v4LUV8W00hOPtM781');
 
 const CheckoutScreen = () => {
 
--- criativcity/pages/checkout/layout.js
+++ criativcity/pages/checkout/layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
 
-const Layout = ({ children }) => (
-  <>
-    <Head>
-      <script type="module" src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.esm.js"></script>
-      <script nomodule src="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk.js"></script>
-      <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
-    </Head>
-    {children}
-  </>
-);
+const Layout = ({ children }) => {
+  useEffect(() => {
+    const loadSDK = () => {
+      const script = document.createElement('script');
+      script.src = 'https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.js';
+      script.defer = true;
+
+      script.onload = () => {
+        console.log('BillDesk SDK loaded');
+        // Any additional logic or function invocations after the SDK is loaded
+      };
+
+      document.head.appendChild(script);
+    };
+
+    loadSDK();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link href="https://uat.billdesk.com/jssdk/v1/dist/billdesksdk/billdesksdk.css" rel="stylesheet" />
+      </Head>
+      {children}
+    </>
+  );
+};
 
 export default Layout;
